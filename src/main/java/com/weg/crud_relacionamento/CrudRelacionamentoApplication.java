639391c172f650a6e3b7f2022ed13b37b7af4ce2
amanda_salvador <amanda_salvador@estudante.sesisenai.org.br>
2026-04-10 14:11:34 -0300

    Feat: Adicionando estruturação base

---
+++ src/main/java/com/weg/crud_relacionamento/CrudRelacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.crud_relacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRelacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRelacionamentoApplication.class, args);
+	}
+
+}
